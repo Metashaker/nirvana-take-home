3b48fd7af965021e07991dc9977240a5bb66d42c
Metashaker <emiliano@territoryfoods.com>
2022-03-29 16:03:33 -0600

    feat() payments post endpoint done and made a promise

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -2,24 +2,31 @@ var express = require('express');
 var router = express.Router();
 const payments = require('../dataStore/paymentsSeed')
 const save = require('../utils/save')
+const uuid = require('uuid')
 /* GET payments listing. */
 router.get('/', function(req, res, next) {
   res.send(payments);
 });
 
-router.post('/', function(req, res, next) {
-  const { therapistSessionId, patientName, date, totalAmount, status } = req.body
-  if (!therapistSessionId || !patientName || !date || !totalAmount || !status) {
-    res.status(422)
-    return new Error('Missing parameters')
-    
+router.post('/', async function(req, res, next) {
+  try {
+    const {therapistSessionId, patientName, date, totalAmount, status } = req.body
+    const paymentId = uuid.v4()
+    if (!therapistSessionId || !patientName || !date || !totalAmount || !status) {
+      res.status(422)
+      return new Error('Missing parameters')
+      
+    }
+    const list = save({ therapistSessionId, paymentId, patientName, date, totalAmount, status },
+      payments, 
+      __dirname + '/../dataStore/paymentsStore.json')
+  
+    res.send(list);
+    res.status(200)
+  } catch(e){
+    console.error(e)
+    //logging would happen here
   }
-  const list = save({ therapistSessionId, patientName, date, totalAmount, status },
-    payments, 
-    __dirname + '/../dataStore/paymentsStore.json')
-
-  res.send(list);
-  res.status(200)
 });
 
 module.exports = router;
