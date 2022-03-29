c8986791e8545265823bb4e86104339931156f5a
Metashaker <emiliano@territoryfoods.com>
2022-03-29 14:36:51 -0600

    feat() all get endpints working correctly

--- backend/routes/therapists.js
+++ backend/routes/therapists.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const therapists = require('../dataStore/therapistsStore')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(therapists);
 });
 
 module.exports = router;
