e9e2641c69c926b928167ca5fcd2a2c4d5f6177a
Metashaker <emiliano@territoryfoods.com>
2022-03-29 17:05:25 -0600

    feat() scaffolded frontend

---
+++ frontend/src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+
+const layoutStyles = css({
+  width: '100vw',
+  height: '100vw',
+  overflow:
+})
+const Layout = () => {
+  return (
+    <div>Hello world</div>
+  )
+}
+ 
+export default Layout
