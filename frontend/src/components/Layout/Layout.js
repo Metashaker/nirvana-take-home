d62b93a2b4c67a44edba2a9ddfb4470a94f47511
Metashaker <emiliano@territoryfoods.com>
2022-03-29 17:20:44 -0600

    feat() basic layout with navbar and footer done

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,13 +1,45 @@
 import { css } from '@emotion/react';
 
-const layoutStyles = css({
+
+  // positions app header + content at top, and
+  // footer at bottom of layout.
+const layoutStyles = {
   width: '100vw',
-  height: '100vw',
-  overflow:
-})
-const Layout = () => {
+  height: '100vh',
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+}
+
+const navbarStyles = {
+  width: '100%',
+  backgroundColor: 'grey',
+  height: '4rem',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-evenly'
+}
+const footerStyles = {
+  bottom: 0,
+  backgroundColor: 'grey',
+  width: '100%',
+  height: '2.5rem'
+}
+
+const Layout = ({children}) => {
   return (
-    <div>Hello world</div>
+    <div style={layoutStyles}>
+      <div style={navbarStyles}>
+        <p>New session</p>
+        <p>Previous sessions</p> 
+        
+        </div>
+      <div>{children}</div>
+      <div style={footerStyles}>
+        <p style={{margin: '0 auto'}}>Footer</p>
+      </div>
+    </div>
   )
 }
  
