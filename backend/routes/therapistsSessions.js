efa805bca746e3e1c2713f87b2651b41889ea58e
Metashaker <emiliano@territoryfoods.com>
2022-03-29 18:18:06 -0600

    efat() adds requirement in backend if fee <= 0

--- backend/routes/therapistsSessions.js
+++ backend/routes/therapistsSessions.js
@@ -16,13 +16,15 @@ router.get('/', async function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   try {
-    const {therapistId, patientName, totalAmount, status, date } = req.body
+    const {therapistId, patientName, totalAmount, status, date, fee } = req.body
     const therapistSessionId = uuid.v4()
     //if I had more time, the totalAmount would be based off the sum of the payments that match the therapistSessionId
-    if (!therapistId || !patientName || !totalAmount || !status || !date) {
+    if (!therapistId || !patientName || !totalAmount || !status || !date || !fee) {
       res.status(422)
       return new Error('Missing parameters')
-      
+    }
+    if ( fee <= 0) {
+      return new Error('Enter a fee greater than 0')
     }
     const list = save({therapistId, therapistSessionId , patientName, date, totalAmount, status },
       therapistSessions, 
